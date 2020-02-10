fa74cf9d6a1ec4ccc708a2d737ee60c39270c36d
GustavoHassen <gustavo.hassen91@gmail.com>
2020-02-10 16:13:03 -0300

    Initial commit

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'aaa656a4bb2edc',
+    pass: '63fe7236f1b15f',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
